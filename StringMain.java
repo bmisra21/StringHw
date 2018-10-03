abe870c0c5b0e1f2e4d61b0b69c18afdbba2d960
bmisra21 <42977074+bmisra21@users.noreply.github.com>
2018-10-03 13:35:54 -0400

    Add files via upload

---
+++ StringMain.java
@@ -0,0 +1,15 @@
+
+public class StringMain 
+{
+	/*Write a short program (Main method only) which will prompt the 
+	 * user to enter their name and a 10 digit telephone number in 
+	 * the format xxxxxxxxxx and will use substrings 
+	 * to return the same phone number in the format (xxx) xxx-xxxx.   
+	 * Print the name using printf so the name takes up 20 characters left 
+	 * justified followed by the phone number.
+	 */
+
+
+//Print out your code and console output to turn in tomorrow
+
+}
