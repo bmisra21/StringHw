94a49d3c9ddaef024b7769fb48bce377ddf020c3
bmisr <bmisr@DESKTOP-J3KHLGD.fios-router.home>
2018-10-03 21:07:35 -0400

    commit1

--- StringMain.java
+++ StringMain.java
@@ -1,15 +1,29 @@
+//@author bmisra21 (Bharavi Misra)
+//StringHW
+//Mr. Ellis P6
 
+import java.util.Scanner;
 public class StringMain 
 {
-	/*Write a short program (Main method only) which will prompt the 
-	 * user to enter their name and a 10 digit telephone number in 
-	 * the format xxxxxxxxxx and will use substrings 
-	 * to return the same phone number in the format (xxx) xxx-xxxx.   
-	 * Print the name using printf so the name takes up 20 characters left 
-	 * justified followed by the phone number.
-	 */
 
-
-//Print out your code and console output to turn in tomorrow
-
+	public static void main (String[] args) 
+	{
+		Scanner in =  new Scanner(System.in);
+		
+		System.out.print("Enter your full name: ");
+		String fname = in.next();
+		String lname = in.next();
+		String fullName = fname + lname;
+		
+		System.out.print("\nEnter a ten digit phone number: ");
+		String phoneNumber = in.next();
+		String areaCode = "(" + phoneNumber.substring(0,3) + ")";
+		String prefix  = "-" + phoneNumber.substring(3,6) + "-";
+		String lineNumber = phoneNumber.substring(6,10);
+		String fullNumber = areaCode + prefix + lineNumber;
+		
+		System.out.printf("\n%-20s %s \n%s %s ", "Name: ", fullName, "Phone Number: ", fullNumber );
+		
+	}
+	
 }
